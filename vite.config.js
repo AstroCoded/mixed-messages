b81c961e4a334780bc22757b760c7ce0ae24a9a9
Eric Emmons <eric.emmons@gmail.com>
2025-10-21 19:44:56 -0400

    adding deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/mixed-messages/',
   plugins: [react()],
-  build: { outDir: 'dist' }
-});
+  build: { outDir: 'dist' },
+})
